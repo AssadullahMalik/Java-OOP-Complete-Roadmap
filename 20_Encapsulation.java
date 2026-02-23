4f75e1fe351cccab59cb55e83522f2602b6c0b9f
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-23 21:28:17 +0500

    Implement encapsulation in Person class

---
+++ 20_Encapsulation.java
@@ -0,0 +1,32 @@
+class Person {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        if (age > 0)
+            this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+public class 20_Encapsulation {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setName("John");
+        p.setAge(25);
+
+        System.out.println("Name: " + p.getName());
+        System.out.println("Age: " + p.getAge());
+    }
+}
