61a6db72f0468399b28ca3031ccafedde9b75312
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-23 21:30:35 +0500

    Add BasicException class to handle division by zero

---
+++ 25_BasicException.java
@@ -0,0 +1,14 @@
+public class 25_BasicException {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+            int result = a / b;
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero!");
+        } finally {
+            System.out.println("Execution Completed");
+        }
+    }
+}
