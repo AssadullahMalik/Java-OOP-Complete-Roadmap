322b9a2bb1cd3723a87d408ec707eb79e612f133
AssadullahMalik <asadullahmk102@gmail.com>
2026-02-23 21:38:00 +0500

    Add Runnable implementation for threading example

---
+++ 30_ThreadByRunnable.java
@@ -0,0 +1,20 @@
+class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Runnable Thread Count: " + i);
+        }
+    }
+}
+
+public class ThreadUsingRunnable {
+
+    public static void main(String[] args) {
+
+        MyRunnable obj = new MyRunnable();
+
+        Thread t1 = new Thread(obj);
+        t1.start();
+    }
+}
